c4d984276b178e2ffc00f86d0a9e6cef74931de0
khvci <88493840+khvci@users.noreply.github.com>
2024-10-16 22:57:14 +0300

    correct directory names and paths

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import Tools.ResultPrinter;
-import Tools.TopDomainsFinder;
-import Tools.TxtToStringArrayConverter;
+import tools.ResultPrinter;
+import tools.TopDomainsFinder;
+import tools.TxtToStringArrayConverter;
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         HashSet<String> emails = TxtToStringArrayConverter
-                .convertTxtToStringSet("src/DomainsList.txt");
+                .convertTxtToStringSet("src/data/DomainsList.txt");
 
         HashSet<String> whitelist = TxtToStringArrayConverter
-                .convertTxtToStringSet("src/WhitelistedDomains.txt");
+                .convertTxtToStringSet("src/data/WhitelistedDomains.txt");
 
         HashSet<String> topThreeDomains = TopDomainsFinder
                 .getTopThreeDomains(emails, whitelist);
